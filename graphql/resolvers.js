81c7df1c32308fff5de611c896feb9249accbdd8
Seunghwan <seunghwansohn@gmail.com>
2020-09-25 20:38:22 +0700

    3. 모두 조회와 filter값 받기 두가지로 query 구현

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -14,14 +14,25 @@
 
 //라는 아주 간단한 응답을 하는 api 상태
 
-const nicolas = {
-  name : "Nicolas",
-  age : 18,
-  gender : "female"
-}
+const people = [
+  {
+    id : 0,
+    name : "Nicolas",
+    age : 18,
+    gender : "female"
+  },
+  {
+    id : 1,
+    name : "David",
+    age : 21,
+    gender : "male"
+  }
+]
+
 const resolvers = {
   Query: {
-    person: () => nicolas
+    people: () => people,
+    person: (_, {id}) => people.filter(person => person.id === id)[0]
   }
 }
 
